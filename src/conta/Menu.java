a15bc8dfcd88280b4d55b8f6b9779adc04dcee59
Breno Menezes <breno.luizmenezes0123@gmail.com>
2023-11-17 12:16:20 -0300

    Classe Contas adicionada

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,11 +2,19 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
 		
 		Scanner leia = new Scanner(System.in);
 		
