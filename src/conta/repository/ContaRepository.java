7a0c0670da2e9cf893f1c930c0c1c59db82c3c38
Breno Menezes <breno.luizmenezes0123@gmail.com>
2023-11-23 13:43:24 -0300

    Conta Repository criado

---
+++ src/conta/repository/ContaRepository.java
@@ -0,0 +1,16 @@
+package conta.repository;
+
+import conta.model.Conta;
+
+public interface ContaRepository {
+
+	public void procurarPorNumero(int numero);
+	public void listarTodas();
+	public void cadastrar(Conta conta);
+	public void atualizar(Conta conta);
+	public void deletar(int numero);
+	
+	public void sacar(int numero, float valor);
+	public void depositar(int numero, float valor);
+	public void transferir(int numeroOrigem, int numeroDestino, float valor);
+}
